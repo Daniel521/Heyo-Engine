153fd82d466161d0006b68194fb709f420f30e32
Daniel Verdugo <daniel.verdugo521@gmail.com>
2018-01-12 22:31:35 -0800

    Good stopping point for Character.

--- HeyoEngine/Heyo/h_Character.cpp
+++ HeyoEngine/Heyo/h_Character.cpp
@@ -3,446 +3,307 @@
 #include <iostream>
 using namespace std;
 
-#define gravity 80
+#define gravity 70
 
-	//Character::Character()
-	//	//sprite(NULL),
-	//	//sprite_rect({ 0,0,0,0 }),
-	//	//curr_frame(0),
-	//	//x(0),
-	//	//y(0),
-	//	//x_speed(100.0f),
-	//	//y_speed(100.0f),
-	//	//is_jumping(false),
-	//	//width(20),
-	//	//height(20),
-	//	//spr_range_idle({ -1,-1 }),
-	//	//spr_range_moveLeft({ -1,-1 }),
-	//	//spr_range_moveRight({ -1,-1 }),
-	//	//spr_range_jump({-1,-1})
-	//{
-	//	sprite = NULL;
-	//	sprite_rect = { 0,0,0,0 };
-	//	curr_frame = 0;
-	//	x = 0;
-	//	y = 0;
-	//	x_speed = 100.0f;
-	//	y_speed = 100.0f;
-	//	jumping = false;
-	//	width = 20;
-	//	height = 20;
-	//	spr_range_idle = { -1,0 };
-	//	spr_range_move = { -1,0 };
-	//	spr_range_jump = { -1,0 };
-
-	//}
-
-	//Character::~Character()
-	//{
-	//	if (sprite != NULL)
-	//	{
-	//		delete sprite;
-	//	}
-	//}
-
-	//bool Character::loadSprite(std::string addresss, int indiWidth, int indiHeight)
-	//{
-	//	if (sprite != NULL)
-	//	{
-	//		delete sprite;
-	//		sprite = NULL;
-	//	}
-	//	sprite = new Heyo::Sprite(Heyo::Engine->graphics);
-	//	sprite_rect = {static_cast<int>(x),static_cast<int>(*Heyo::Engine->graphics->SCREEN_HEIGHT - (*Heyo::Engine->graphics->SCREEN_HEIGHT * .08f) - height),width,height };
-
-	//	return sprite->loadSprite(addresss, indiWidth, indiHeight);
-
-	//	// we'll use this for main characters like greg, but for now, no
-	//	//sprite_rect = { *Heyo::Engine->graphics->SCREEN_WIDTH - width/2,*Heyo::Engine->graphics->SCREEN_HEIGHT - (*Heyo::Engine->graphics->SCREEN_HEIGHT * .08f) - height,width,height };
-	//	// only difference is the x
-	//}
-
-	//void Character::update()
-	//{
-	//	static float time_since_last_anim = 0;
-	//	time_since_last_anim += Heyo::Engine->timer->seconds;
-
-	//	// Jumping is happening
-	//	if (y_speed != 0)
-	//	{
-	//		jumpUpdate();
-	//	//	jumpAnim(time_since_last_anim);
-	//	}
-	//	//// Walking is happening
-	//	//else if (x_speed != 0)
-	//	//{
-	//	//	walkAnim(time_since_last_anim);
-	//	//}
-	//	//// Idle is happening
-	//	//else
-	//	//{
-	//	//	idleAnim(time_since_last_anim);
-	//	//	curr_frame_walk = sp_greg_walk.min;
-	//	//}
-
-	//	//sprite_rect = { *Heyo::Engine->graphics->SCREEN_WIDTH - width / 2,*Heyo::Engine->graphics->SCREEN_HEIGHT - (*Heyo::Engine->graphics->SCREEN_HEIGHT * .08f) - height,width,height };
-	//	//sprite_rect = { static_cast<int>(x),static_cast<int>(*Heyo::Engine->graphics->SCREEN_HEIGHT - (*Heyo::Engine->graphics->SCREEN_HEIGHT * .08f) - height),width,height };
-	//	sprite_rect.x = static_cast<int>(x);
-	//	sprite_rect.y = static_cast<int>(*Heyo::Engine->graphics->SCREEN_HEIGHT - (*Heyo::Engine->graphics->SCREEN_HEIGHT * .08f) - height - y);
-	//	sprite_rect.w = width;
-	//	sprite_rect.h = height;
-	//}
-
-	//void Character::moveLeft()
-	//{
-	//	if (sprite->isFlippedHor() == false)
-	//	{
-	//		sprite->flipHor();
-	//	}
-	//	x -= x_speed * Heyo::Engine->timer->seconds;
-	//	//sprite_rect.x = static_cast<int>(x);
-	//}
-
-	//void Character::moveRight()
-	//{
-	//	if (sprite->isFlippedHor() == true)
-	//	{
-	//		sprite->flipHor();
-	//	}
-	//	x += x_speed * Heyo::Engine->timer->seconds;
-	//	sprite_rect.x = static_cast<int>(x);
-	//}
-
-	//void Character::jump()
-	//{
-	//	y_curr_speed = y_speed;
-	//	jumping = true;
-	//	// $$FALL SOUND
-	//}
-
-	//void Character::setX(float x)
-	//{
-	//	this->x = x;
-	//}
-
-	//void Character::setY(float y)
-	//{
-	//	this->y = y;
-	//}
-
-	//float Character::getX()
-	//{
-	//	return x;
-	//}
-
-	//float Character::getY()
-	//{
-	//	return y;
-	//}
-
-	//void Character::setX_Speed(float x_speed)
-	//{
-	//	this->x_speed = x_speed;
-	//}
-
-	//void Character::setY_Speed(float y_speed)
-	//{
-	//	this->y_speed = y_speed;
-	//}
-
-	//float Character::getX_Speed()
-	//{
-	//	return x_speed;
-	//}
-
-	//float Character::getY_Speed()
-	//{
-	//	return y_speed;
-	//}
-
-	//void Character::setIdleRange(Heyo::Range range)
-	//{
-	//	this->spr_range_idle = range;
-	//}
-
-	//void Character::setMoveRange(Heyo::Range range)
-	//{
-	//	this->spr_range_move = range;
-	//}
-
-	//void Character::setJumpRange(Heyo::Range range)
-	//{
-	//	this->spr_range_jump = range;
-	//}
-
-	//bool Character::isJumping()
-	//{
-	//	return jumping;
-	//}
-
-	//void Character::jumpUpdate()
-	//{
-	//	y_curr_speed -= 1400 * Heyo::Engine->timer->seconds;
-
-	//	y += y_curr_speed * Heyo::Engine->timer->seconds;
-	//	if (y <= 0)
-	//	{
-	//		y = 0;
-	//		y_curr_speed = 0;
-	//		jumping = false;
-	//	}
-	//}
-
-Character::Character()
+namespace Heyo_Platform
 {
-	sprite = NULL;
-	for (int i = 0; i < 4; i++)
-	{
-		spr_anim[i].min = -1;
-		anim_period[i] = .2f;
-	}
-	curr_frame = 0;
-	x = 10.0f;
-	y = 0.0f;
-	x_speed = 500.0f;
-	y_speed = 0.0f;
-	spr_rect.x = static_cast<int>(x);
-	spr_rect.y = static_cast<int>(400);
-	spr_rect.h = 100;
-	spr_rect.w = 100;
-	ground = 300;
-	jump_speed = 22;
-	jumping = false;
-	walking = 0;
-}
 
-Character::~Character()
-{
-	if (sprite != NULL)
+	Character::Character()
 	{
-		delete sprite;
-	}
-}
-
-bool Character::loadSprite(std::string address, int indiWidth, int indiHeight)
-{
-	if (sprite != NULL)
-	{
-		delete sprite;
 		sprite = NULL;
-	}
-	sprite = new Heyo::Sprite(Heyo::Engine->graphics);
-	return sprite->loadSprite(address, indiWidth, indiHeight);
-}
-
-void Character::moveLeft()
-{
-	x -= x_speed * Heyo::Engine->timer->seconds;
-	walking = 1;
-}
-
-void Character::moveRight()
-{
-	x += x_speed * Heyo::Engine->timer->seconds;
-	walking = 2;
-}
-
-void Character::jump()
-{
-	jumping = true;
-	y_speed = jump_speed;
-	if (spr_anim[2].min >= 0)
-	{
-		curr_frame = spr_anim[2].min;
-	}
-}
-
-void Character::draw()
-{
-	Heyo::Engine->graphics->update(*sprite, spr_rect);
-}
-
-void Character::update()
-{
-	if (jumping == true || y != 0)
-	{
-		jumpAnim();
-		jumpUpdate();
-	}
-	else
-	{
-		if (walking == 1)
+		for (int i = 0; i < 4; i++)
 		{
-			// walk left
+			spr_anim[i].min = -1;
+			anim_period[i] = .2f;
 		}
-		else if (walking == 2)
+		curr_frame = 0;
+		x = 10.0f;
+		y = 0.0f;
+		x_speed = 400.0f;
+		y_speed = 0.0f;
+		spr_rect.x = static_cast<int>(x);
+		spr_rect.y = static_cast<int>(400);
+		spr_rect.h = 100;
+		spr_rect.w = 100;
+		ground = 300;
+		jump_speed = 22;
+		jumping = false;
+		walking = 0;
+	}
+
+	Character::~Character()
+	{
+		if (sprite != NULL)
 		{
-			// walk right
+			delete sprite;
+		}
+	}
+
+	bool Character::loadSprite(std::string address, int indiWidth, int indiHeight)
+	{
+		if (sprite != NULL)
+		{
+			delete sprite;
+			sprite = NULL;
+		}
+		sprite = new Heyo::Sprite(Heyo::Engine->graphics);
+		return sprite->loadSprite(address, indiWidth, indiHeight);
+	}
+
+	void Character::moveLeft()
+	{
+		if (sprite->isFlippedHor() == false)
+		{
+			sprite->flipHor();
+		}
+		x -= x_speed * Heyo::Engine->timer->seconds;
+		walking = 1;
+	}
+
+	void Character::moveRight()
+	{
+		if (sprite->isFlippedHor() == true)
+		{
+			sprite->flipHor();
+		}
+		x += x_speed * Heyo::Engine->timer->seconds;
+		walking = 2;
+	}
+
+	void Character::jump()
+	{
+		jumping = true;
+		y_speed = jump_speed;
+		if (spr_anim[2].min >= 0)
+		{
+			curr_frame = spr_anim[2].min;
+		}
+	}
+
+	void Character::draw(bool show_box)
+	{
+		if (show_box == true)
+		{
+			Heyo::Engine->graphics->drawRect(spr_rect, true, 255, 200, 50);
+		}
+		Heyo::Engine->graphics->update(*sprite, spr_rect);
+	}
+
+	void Character::update()
+	{
+		if (jumping == true || y != 0)
+		{
+			jumpAnim();
+			jumpUpdate();
 		}
 		else
 		{
-			// idle
-			idleAnim();
+			if (walking == 1)
+			{
+				// walk left
+				walkAnim();
+			}
+			else if (walking == 2)
+			{
+				// walk right
+				walkAnim();
+			}
+			else
+			{
+				// idle
+				idleAnim();
+			}
+		}
+
+		walking = 0;
+		spr_rect.x = static_cast<int>(x);
+		spr_rect.y = static_cast<int>(ground - spr_rect.h - y);
+	}
+
+	void Character::setIdleAnim(int first, int last)
+	{
+		spr_anim[0] = { first,last };
+	}
+
+	void Character::setJumpAnim(int first, int last)
+	{
+		spr_anim[2].min = first;
+		spr_anim[2].max = last;
+	}
+
+	void Character::setWalkAnim(int first, int last)
+	{
+		spr_anim[1].min = first;
+		spr_anim[1].max = last;
+	}
+
+	void Character::setHeight(int height)
+	{
+		spr_rect.h = height;
+	}
+
+	void Character::setWidth(int width)
+	{
+		spr_rect.w = width;
+	}
+
+	void Character::setX(float x)
+	{
+		this->x = x;
+	}
+
+	void Character::setY(float y)
+	{
+		this->y = y;
+	}
+
+	void Character::setX_Speed(float x_speed)
+	{
+		this->x_speed = x_speed;
+	}
+
+	void Character::setY_Speeed(float y_speed)
+	{
+		this->y_speed = y_speed;
+	}
+
+	void Character::setJumpSpeed(float jump_speed)
+	{
+		this->jump_speed = jump_speed;
+	}
+
+	bool Character::isJumping()
+	{
+		return jumping;
+	}
+
+	float Character::getJumpSpeed()
+	{
+		return jump_speed;
+	}
+
+	int Character::getGroundLevel()
+	{
+		return ground;
+	}
+
+	float Character::getX()
+	{
+		return x;
+	}
+
+	float Character::getY()
+	{
+		return y;
+	}
+
+	int Character::getHeight()
+	{
+		return spr_rect.h;
+	}
+
+	int Character::getWidth()
+	{
+		return spr_rect.w;
+	}
+
+	float Character::getX_Speed()
+	{
+		return x_speed;
+	}
+
+	float Character::getY_Speed()
+	{
+		return y_speed;
+	}
+
+	void Character::setIdleFramePeriod(float seconds)
+	{
+		anim_period[0] = seconds;
+	}
+
+	void Character::setJumpFramePeriod(float seconds)
+	{
+		anim_period[2] = seconds;
+	}
+
+	void Character::setWalkFramePeriod(float seconds)
+	{
+		anim_period[1] = seconds;
+	}
+
+	/* Helper Functions */
+
+	void Character::jumpUpdate()
+	{
+		y_speed -= gravity * Heyo::Engine->timer->seconds;
+		y += y_speed;
+		if (y <= 0)
+		{
+			y = 0;
+			y_speed = 0;
+			jumping = false;
 		}
 	}
 
-	walking = 0;
-	spr_rect.x = static_cast<int>(x);
-	spr_rect.y = static_cast<int>(ground - spr_rect.h - y);
-}
-
-void Character::setIdleAnim(int first, int last)
-{
-	spr_anim[0] = { first,last };
-}
-
-void Character::setJumpAnim(int first, int last)
-{
-	spr_anim[2].min = first;
-	spr_anim[2].max = last;
-}
-
-void Character::setHeight(int height)
-{
-	spr_rect.h = height;
-}
-
-void Character::setWidth(int width)
-{
-	spr_rect.w = width;
-}
-
-void Character::setX(float x)
-{
-	this->x = x;
-}
-
-void Character::setY(float y)
-{
-	this->y = y;
-}
-
-void Character::setX_Speed(float x_speed)
-{
-	this->x_speed = x_speed;
-}
-
-void Character::setY_Speeed(float y_speed)
-{
-	this->y_speed = y_speed;
-}
-
-void Character::setJumpSpeed(float jump_speed)
-{
-	this->jump_speed = jump_speed;
-}
-
-bool Character::isJumping()
-{
-	return jumping;
-}
-
-float Character::getJumpSpeed()
-{
-	return jump_speed;
-}
-
-int Character::getGroundLevel()
-{
-	return ground;
-}
-
-float Character::getX()
-{
-	return x;
-}
-
-float Character::getY()
-{
-	return y;
-}
-
-int Character::getHeight()
-{
-	return spr_rect.h;
-}
-
-int Character::getWidth()
-{
-	return spr_rect.w;
-}
-
-float Character::getX_Speed()
-{
-	return x_speed;
-}
-
-float Character::getY_Speed()
-{
-	return y_speed;
-}
-
-void Character::setIdleFramePeriod(float seconds)
-{
-	anim_period[0] = seconds;
-}
-
-void Character::setJumpFramePeriod(float seconds)
-{
-	anim_period[2] = seconds;
-}
-
-/* Helper Functions */
-
-void Character::jumpUpdate()
-{
-	y_speed -= gravity * Heyo::Engine->timer->seconds;
-	y += y_speed;
-	if (y <= 0)
+	void Character::idleAnim()
 	{
-		y = 0;
-		y_speed = 0;
-		jumping = false;
+		if (spr_anim[0].min < 0)
+			return;
+		static float time_since_last_anim = 0;
+		time_since_last_anim += Heyo::Engine->timer->seconds;
+		if (curr_frame < spr_anim[0].min || curr_frame >= spr_anim[0].max)
+		{
+			curr_frame = spr_anim[0].min;
+		}
+		if (time_since_last_anim >= anim_period[0])
+		{
+			time_since_last_anim = 0;
+			curr_frame++;
+		}
+		sprite->swap(curr_frame);
 	}
-}
 
-void Character::idleAnim()
-{
-	if (spr_anim[0].min < 0)
-		return;
-	static float time_since_last_anim = 0;
-	time_since_last_anim += Heyo::Engine->timer->seconds;
-	if (curr_frame < spr_anim[0].min || curr_frame >= spr_anim[0].max)
+	void Character::jumpAnim()
 	{
-		curr_frame = spr_anim[0].min;
+		if (spr_anim[2].min < 0)
+			return;
+		static float time_since_last_anim = 0;
+		time_since_last_anim += Heyo::Engine->timer->seconds;
+		if (curr_frame >= spr_anim[2].max)
+		{
+			return;
+		}
+		if (time_since_last_anim >= anim_period[2])
+		{
+			time_since_last_anim = 0;
+			curr_frame++;
+		}
+		sprite->swap(curr_frame);
 	}
-	if (time_since_last_anim >= anim_period[0])
-	{
-		time_since_last_anim = 0;
-		curr_frame++;
-	}
-	sprite->swap(curr_frame);
-}
 
-void Character::jumpAnim()
-{
-	if (spr_anim[2].min < 0)
-		return;
-	static float time_since_last_anim = 0;
-	time_since_last_anim += Heyo::Engine->timer->seconds;
-	if (curr_frame >= spr_anim[2].max)
+	void Character::walkAnim()
 	{
-		return;
-	}
-	if (time_since_last_anim >= anim_period[2])
-	{
-		time_since_last_anim = 0;
-		curr_frame++;
-	}
-	sprite->swap(curr_frame);
-}
+		if (spr_anim[1].min < 0)
+			return;
+		static float time_since_last_anim = 0;
+		time_since_last_anim += Heyo::Engine->timer->seconds;
+		if (curr_frame < spr_anim[1].min || curr_frame >= spr_anim[1].max)
+		{
+			curr_frame = spr_anim[1].min;
+		}
+		if (time_since_last_anim >= anim_period[1])
+		{
+			time_since_last_anim = 0;
+			curr_frame++;
+		}
+		sprite->swap(curr_frame);
 
-void Character::setGroundLevel(int y)
-{
-	ground = y;
+	}
+
+	void Character::setGroundLevel(int y)
+	{
+		ground = y;
+	}
+
 }
--- HeyoEngine/Heyo/h_Character.h
+++ HeyoEngine/Heyo/h_Character.h
@@ -5,182 +5,104 @@
 
 // Base class used to make all my characters, for now atleast.
 
-
-// Classes used to make characters for a platform game, idk how to spell
-
-//class Character
-//{
-//public:
-//	Heyo::Sprite * sprite;
-//
-//	// sprite_rect w/ respect to the screen, NOT THE MAP!
-//	Heyo::Rect sprite_rect;
-//
-//	int curr_frame;
-//	float y_curr_speed;
-//	float x;
-//	float y;
-//	float x_speed;
-//	float y_speed;
-//	bool jumping;
-//	int width;
-//	int height;
-//	Heyo::Range spr_range_idle;
-//	Heyo::Range spr_range_move;
-//	Heyo::Range spr_range_jump;
-//
-//public:
-//	Character();
-//	~Character();
-//
-//	// - Loads a sprite from the specified addresss, ex: Data/Sprite/greg.png
-//	// - Only loads png for now
-//	// - indiWidth and indiHeight specifies the width and height of each
-//	//	individual sprite in the whole spritesheet, leave as -1 to cover the
-//	//	whole width or height.
-//	bool loadSprite(std::string addresss, int indiWidth = -1, int indiHeight = -1);
-//
-//	// Specifies the current frame of the sprite
-//	//Uint32 getCurrentFrame();
-//
-//	void update();
-//
-//	void moveLeft();
-//
-//	void moveRight();
-//
-//	void jump();
-//
-//	void setX(float x);
-//
-//	void setY(float y);
-//
-//	float getX();
-//
-//	float getY();
-//
-//	void setX_Speed(float x_speed);
-//
-//	void setY_Speed(float y_speed);
-//
-//	float getX_Speed();
-//
-//	float getY_Speed();
-//
-//	void setIdleRange(Heyo::Range range);
-//
-//	void setMoveRange(Heyo::Range range);
-//
-//	void setJumpRange(Heyo::Range range);
-//
-//	bool isJumping();
-//
-//private:
-//
-//	void jumpUpdate();
-//
-//	//void idleAnim();
-//	
-//	//void moveLeftAnim();
-//
-//	//void moveRightAnim();
-//
-//	//void jumpAnim();
-//
-//};
-
-class Character
+namespace Heyo_Platform
 {
-public:
+	// Classes used to make characters for a platform game, idk how to spell
 
-	Heyo::Sprite * sprite;
-	// 0. idle, 1. walk, 2. jump, 3. die
-	Heyo::Range spr_anim[4];
-	// 0. idle, 1. walk, 2. jump, 3. die
-	float anim_period[4];
-	int curr_frame;
+	class Character
+	{
+	public:
 
-	Heyo::Rect spr_rect;
+		Heyo::Sprite * sprite;
+		// 0. idle, 1. walk, 2. jump, 3. die
+		Heyo::Range spr_anim[4];
+		// 0. idle, 1. walk, 2. jump, 3. die
+		float anim_period[4];
+		short curr_frame;
 
-	float x;
-	float y;
-	float x_speed;
-	float jump_speed;
-	float y_speed;
-	// 0. idle, 1. left, 2. right
-	unsigned char walking;
-	bool jumping;
+		Heyo::Rect spr_rect;
+
+		float x;
+		float y;
+		float x_speed;
+		float jump_speed;
+		float y_speed;
+		// 0. idle, 1. left, 2. right
+		unsigned char walking;
+		bool jumping;
 	
-	int ground;
+		int ground;
 
-public:
+	public:
 
-	Character();
-	~Character();
+		Character();
+		~Character();
 
-	bool loadSprite(std::string address, int indiWidth = -1, int indiHeight = -1);
-
-
-	void moveLeft();
-	void moveRight();
-	void jump();
-
-	void draw();
-	void update();
-
-public:
-	// Mutators and Accessors
-
-	// Specifies the speed from what the character
-	// jumps from.
-	void setJumpSpeed(float jump_speed);
-
-	// this function specifies how many frames from top to ground
-	// is the ground located at.
-	void setGroundLevel(int y);
-
-	// specifies the frames that are associated to idle anim
-	// frame 0 is the first frame, like with arrays.
-	void setIdleAnim(int first, int last);
-	void setJumpAnim(int first, int last);
-
-	// set the amount of seconds a frame is shown
-	void setIdleFramePeriod(float seconds);
-	void setJumpFramePeriod(float seconds);
-
-	void setHeight(int height);
-	void setWidth(int width);
-
-	void setX(float x);
-	void setY(float y);
-
-	void setX_Speed(float x_speed);
-	void setY_Speeed(float y_speed);
+		bool loadSprite(std::string address, int indiWidth = -1, int indiHeight = -1);
 
 
-	bool isJumping();
+		void moveLeft();
+		void moveRight();
+		void jump();
 
-	float getJumpSpeed();
-	int getGroundLevel();
+		void draw(bool show_box = false);
+		void update();
 
-	float getX();
-	float getY();
+	public:
+		// Mutators and Accessors
 
-	int getHeight();
-	int getWidth();
+		// Specifies the speed from what the character
+		// jumps from.
+		void setJumpSpeed(float jump_speed);
 
-	float getX_Speed();
-	float getY_Speed();
+		// this function specifies how many frames from top to ground
+		// is the ground located at.
+		void setGroundLevel(int y);
 
+		// specifies the frames that are associated to idle anim
+		// frame 0 is the first frame, like with arrays.
+		void setIdleAnim(int first, int last);
+		void setJumpAnim(int first, int last);
+		void setWalkAnim(int first, int last);
 
+		// set the amount of seconds a frame is shown
+		void setIdleFramePeriod(float seconds);
+		void setJumpFramePeriod(float seconds);
+		void setWalkFramePeriod(float seconds);
 
+		void setHeight(int height);
+		void setWidth(int width);
 
-private:
-	// Function helpers these functions are used to help my other functions
-	void jumpUpdate();
+		void setX(float x);
+		void setY(float y);
 
-	void idleAnim();
+		void setX_Speed(float x_speed);
+		void setY_Speeed(float y_speed);
 
-	void jumpAnim();
+		bool isJumping();
 
-};
+		float getJumpSpeed();
+		int getGroundLevel();
+
+		float getX();
+		float getY();
+
+		int getHeight();
+		int getWidth();
+
+		float getX_Speed();
+		float getY_Speed();
+
+	private:
+		// Function helpers these functions are used to help my other functions
+		void jumpUpdate();
+
+		void idleAnim();
+
+		void jumpAnim();
+
+		void walkAnim();
+
+	};
+
+}
